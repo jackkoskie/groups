c52fa19dda13dc852e2ef4d2231fbf6f1a3a8bc2
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-12-07 12:30:41 -0500

    feat: add user add/remove

--- src/routes/groups/[id]/+page.svelte
+++ src/routes/groups/[id]/+page.svelte
@@ -6,6 +6,7 @@
 	import type { PageData } from './$types';
 	import GroupIcon from '$lib/icons/GroupIcon.svelte';
 	import DeleteIcon from '$lib/icons/DeleteIcon.svelte';
+	import AddUserIcon from '$lib/icons/AddUserIcon.svelte';
 
 	let { data }: { data: PageData } = $props();
 
@@ -17,6 +18,12 @@
 	});
 
 	let groups: Group[] = $state([]);
+	const updateGroup = (newGroup: Group) => {
+		groups = JSON.parse(localStorage.getItem('groups')!);
+		group = newGroup;
+		groups[newGroup.id - 1] = newGroup;
+		localStorage.setItem('groups', JSON.stringify(groups));
+	};
 
 	let isInfoModalOpen = $state(false);
 	// svelte-ignore state_referenced_locally
@@ -28,9 +35,7 @@
 	const closeInfoModal = () => {
 		group.name = tempTitle;
 		group.description = tempDescription;
-		groups[data.id - 1].name = tempTitle;
-		groups[data.id - 1].description = tempDescription;
-		localStorage.setItem('groups', JSON.stringify(groups));
+		updateGroup(group);
 		isInfoModalOpen = false;
 	};
 
@@ -40,8 +45,27 @@
 	};
 	const deleteGroup = () => {
 		groups = groups.filter((g) => g.id !== group.id);
-		localStorage.setItem('groups', JSON.stringify(groups));
 		goto('/groups');
+	};
+
+	let individualToAdd = $state('');
+	let addIndividual = () => {
+		group.individuals.push(individualToAdd);
+		individualToAdd = '';
+		updateGroup(group);
+	};
+	let isIndividualModalOpen = $state(false);
+	let newLineIndividuals = $state('');
+	const toggleIndividualModal = () => {
+		if (isIndividualModalOpen) {
+			group.individuals = newLineIndividuals.split('\n').filter((i) => i !== '');
+			updateGroup(group);
+		} else {
+			newLineIndividuals = group.individuals.join('\n');
+		}
+
+		isIndividualModalOpen = !isIndividualModalOpen;
+		updateGroup(group);
 	};
 
 	onMount(() => {
@@ -51,6 +75,7 @@
 				group = groups.find((g) => g.id === data.id)!;
 				tempTitle = group.name;
 				tempDescription = group.description || '';
+				newLineIndividuals = group.individuals.join('\n');
 			} else {
 				goto('/groups');
 			}
@@ -61,11 +86,48 @@
 <div class="container mx-auto min-h-[calc(100vh-4.22rem)]">
 	<div class="flex items-end gap-3">
 		<h1 class="pt-5 text-3xl font-semibold">{group.name}</h1>
-		<button class="ms-auto text-neutral-600" onclick={openInfoModal}><EditIcon /></button>
+		<button class="ms-auto text-success" onclick={toggleIndividualModal}><AddUserIcon /></button>
+		<button class=" text-neutral-600" onclick={openInfoModal}><EditIcon /></button>
 		<button class="text-xl text-error" onclick={toggleDeleteModal}><DeleteIcon /></button>
 	</div>
 	<p class="mt-3 text-sm text-neutral-400">{group.description}</p>
 	<div class="divider"></div>
+
+	<div>
+		{#if group.individuals.length == 0}
+			<p class="text-neutral-400">No individuals in this group</p>
+		{:else}
+			<h2 class="text-xl font-semibold">Individuals</h2>
+			<ul class="">
+				{#each group.individuals as individual, i}
+					<li class="list-item">
+						<div class="flex flex-row gap-3">
+							<span>-</span>
+							<button
+								class="transition-colors hover:text-error hover:line-through"
+								onclick={() => {
+									group.individuals.splice(i, 1);
+									updateGroup(group);
+								}}>{individual}</button
+							>
+						</div>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+		<form class="form-control mt-3 flex-row gap-2" onsubmit={addIndividual}>
+			<input
+				type="text"
+				placeholder="Add Individual"
+				class="input input-md input-bordered w-full max-w-[15rem]"
+				id="add-individual-text"
+				bind:value={individualToAdd}
+			/>
+			<button class="btn btn-circle btn-outline btn-primary" type="submit">+</button>
+		</form>
+	</div>
+
+	<!-- MODALS ONLY BEYOND THIS POINT -->
 
 	<!-- group name modal -->
 	<dialog class="modal modal-bottom sm:modal-middle" class:modal-open={isInfoModalOpen}>
@@ -115,4 +177,23 @@
 		</div>
 	</dialog>
 	<!-- end delete group modal -->
+
+	<!-- individual modal -->
+	<dialog class="modal modal-bottom sm:modal-middle" class:modal-open={isIndividualModalOpen}>
+		<div class="modal-box">
+			<h3 class="text-lg font-bold">Edit Individuals</h3>
+			<form method="dialog">
+				<div class="label mt-3"><span class="label-text">One Individual Per Line</span></div>
+				<textarea
+					placeholder="Type here"
+					class="input input-bordered min-h-40 w-full resize-y p-4"
+					bind:value={newLineIndividuals}
+				></textarea>
+				<div class="modal-action">
+					<button class="btn" onclick={toggleIndividualModal}>Save</button>
+				</div>
+			</form>
+		</div>
+	</dialog>
+	<!-- end individual modal -->
 </div>
